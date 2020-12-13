6bfa53e2a77604582c9605e2af8548be46f2a798
Виталий Черков <vitaly.cherkov@gmail.com>
2020-12-13 17:01:34 +0300

    fix

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -6,10 +6,12 @@ export default function generateId(
   return a           // if the placeholder was passed, return
     ? (              // a random number from 0 to 15
       // @ts-ignore
+      // eslint-disable-next-line
       a ^            // unless b is 8,
       Math.random()  // in which case
       * 16           // a random number from
       // @ts-ignore
+      // eslint-disable-next-line
       >> a/4         // 8 to 11
     ).toString(16) // in hexadecimal
     : (              // or otherwise a concatenated string:
